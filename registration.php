acc8d6215d59a6c9f45fbaed0f5d041b6543aa58
sudhachauhan <jb708502@gmail.com>
2017-03-07 23:49:28 +0530

    Link for sign in added #69

--- registration.php
+++ registration.php
@@ -52,7 +52,7 @@
   <!--submit button-->
   <div class="div-reg">
      <input class="button" type="submit" value="Create Account">
-    <br><br>
+    <br><br>  <p class = "text">Already have an account?&nbsp <a href ="login.php">  Sign in</a> </p><br><br>
     <img src="images/secure.png" style="width: 20px; height: 20px;"/>
     <div class="text">This is a secure portal</div>
   </div>
